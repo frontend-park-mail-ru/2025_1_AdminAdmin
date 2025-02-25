53e95f5832e76b2b4505214ac0a7cfcc16c3c435
JERR4 <yaroslav.auto@gmail.com>
2025-02-25 20:21:09 +0300

    Fix build.js script

--- build.js
+++ build.js
@@ -1,14 +1,17 @@
 import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
+
+fs.mkdirSync(path.join("public", "build"), { recursive: true });
 
 const commands = [
-    "mkdir -p public/build",
-    "handlebars -m public/src/pages/restaurantPage/restaurantPage.hbs -f public/build/restaurantPage.js",
-    "handlebars -m public/src/pages/restaurantList/restaurantList.hbs -f public/build/restaurantList.js",
-    "handlebars -m public/src/pages/loginPage/loginPage.hbs -f public/build/loginPage.js",
-    "handlebars -m public/src/pages/registerPage/registerPage.hbs -f public/build/registerPage.js",
-    "handlebars -m public/src/components/header/header.hbs -f public/build/header.js",
-    "handlebars -m public/src/components/restaurantCard.hbs -f public/build/restaurantCard.js",
-    "handlebars -m public/src/components/auxHeader/auxHeader.hbs -f public/build/auxHeader.js",
+    `npx handlebars -m ${path.join("public", "src", "pages", "restaurantPage", "restaurantPage.hbs")} -f ${path.join("public", "build", "restaurantPage.js")}`,
+    `npx handlebars -m ${path.join("public", "src", "pages", "restaurantList", "restaurantList.hbs")} -f ${path.join("public", "build", "restaurantList.js")}`,
+    `npx handlebars -m ${path.join("public", "src", "pages", "loginPage", "loginPage.hbs")} -f ${path.join("public", "build", "loginPage.js")}`,
+    `npx handlebars -m ${path.join("public", "src", "pages", "registerPage", "registerPage.hbs")} -f ${path.join("public", "build", "registerPage.js")}`,
+    `npx handlebars -m ${path.join("public", "src", "components", "header", "header.hbs")} -f ${path.join("public", "build", "header.js")}`,
+    `npx handlebars -m ${path.join("public", "src", "components", "restaurantCard.hbs")} -f ${path.join("public", "build", "restaurantCard.js")}`,
+    `npx handlebars -m ${path.join("public", "src", "components", "auxHeader", "auxHeader.hbs")} -f ${path.join("public", "build", "auxHeader.js")}`,
 ];
 
-commands.forEach(cmd => execSync(cmd, { stdio: "inherit" }));
+commands.forEach(cmd => execSync(cmd, { stdio: "inherit", shell: true }));
