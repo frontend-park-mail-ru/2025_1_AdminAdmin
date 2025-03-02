4687d6f913810e37bf25041e990cd3095e4a3472
Jer <yaroslav.auto@gmail.com>
2025-03-02 17:00:32 +0300

    Rebase on develop, solve merge conflicts

--- public/build/header.js
+++ public/build/header.js
@@ -1,10 +1,1 @@
-(() => {
-  var n = Handlebars.template;
-  (Handlebars.templates = Handlebars.templates || {})['header.hbs'] = n({
-    compiler: [8, '>= 4.3.0'],
-    main: function (n, t, r, a, s) {
-      return '\x3c!--<div class="logo">\r\n        Delivery\r\n</div>\r\n<div class="search-form">\r\n    <form action="">\r\n        <label>\r\n            <input class="search-form-input" placeholder="Поиск по ресторанам" type="text" />\r\n        </label>\r\n        <button class="search-form-button">Найти</button>\r\n    </form>\r\n</div>\r\n<button class="address-button">Выберите адрес доставки</button>\r\n<button class="login-button">Войти</button>\r\n<button class="logout-button">Выйти</button>--\x3e\r\n\r\n<div class=\'header\'>\r\n  <div class=\'logo\'>\r\n  </div>\r\n  <div class=\'header__buttons\'>\r\n  </div>\r\n</div>';
-    },
-    useData: !0,
-  });
-})();
+(()=>{var n=Handlebars.template;(Handlebars.templates=Handlebars.templates||{})["header.hbs"]=n({compiler:[8,">= 4.3.0"],main:function(n,t,a,s,e){return'\x3c!--<div class="logo">\n        Delivery\n</div>\n<div class="search-form">\n    <form action="">\n        <label>\n            <input class="search-form-input" placeholder="Поиск по ресторанам" type="text" />\n        </label>\n        <button class="search-form-button">Найти</button>\n    </form>\n</div>\n<button class="address-button">Выберите адрес доставки</button>\n<button class="login-button">Войти</button>\n<button class="logout-button">Выйти</button>--\x3e\n\n<div class=\'header\'>\n  <div class=\'logo\'>\n  </div>\n  <div class=\'header__buttons\'>\n  </div>\n</div>'},useData:!0})})();
--- public/build/restaurantList.js
+++ public/build/restaurantList.js
@@ -1,10 +1,1 @@
-(() => {
-  var a = Handlebars.template;
-  (Handlebars.templates = Handlebars.templates || {})['restaurantList.hbs'] = a({
-    compiler: [8, '>= 4.3.0'],
-    main: function (a, e, t, r, n) {
-      return '<div class="restaurant__container">\r\n\r\n</div>';
-    },
-    useData: !0,
-  });
-})();
+(()=>{var a=Handlebars.template;(Handlebars.templates=Handlebars.templates||{})["restaurantList.hbs"]=a({compiler:[8,">= 4.3.0"],main:function(a,e,t,r,n){return'<div class="restaurant__container">\r\n\r\n</div>'},useData:!0})})();
