900ca224eed50523334272bbfd56ba3d309082bc
rediska123 <larini2004@yandex.ru>
2025-03-04 22:32:36 +0300

    Сделал страницу ресторанов

--- public/src/components/button/button.js
+++ public/src/components/button/button.js
@@ -1,52 +1,55 @@
 /* Кнопка */
 export class button {
-    #parent;        // Родитель (где вызывается)
-    #props = {      // Свойства кнопки
-        id: "",     // Id для идентификации
-        text: "",   // text для отображения
-    };
-    #style;
-    #onSubmit;      // Функция при нажатии
+  #parent; // Родитель (где вызывается)
+  #props = {
+    // Свойства кнопки
+    id: '', // Id для идентификации
+    text: '', // text для отображения
+  };
+  #style;
+  #onSubmit; // Функция при нажатии
 
-    /* Конструктор */
-    constructor(parent, id, text, onSubmit, style="button") {
-        this.#parent = parent;
-        this.#props = {id: id, text: text};
-        this.#onSubmit = onSubmit;
-        this.#style = style;
+  /* Конструктор */
+  constructor(parent, id, text, onSubmit, style = 'button') {
+    this.#parent = parent;
+    this.#props = { id: id, text: text };
+    this.#onSubmit = onSubmit;
+    this.#style = style;
+  }
+
+  /* Ссылка на объект */
+  get self() {
+    return document.getElementById(this.#props.id);
+  }
+
+  /* Действие при нажатии */
+  #handleClick() {
+    if (this.#onSubmit !== undefined) {
+      this.self.addEventListener('click', (event) => {
+        event.preventDefault(); // Отменяем дефолтное дейтсвие
+        this.#onSubmit(); // Вызываем новое
+      });
     }
+  }
 
-    /* Ссылка на объект */
-    get self(){
-        return document.getElementById(this.#props.id);
-    } 
+  /* Рендер */
+  render() {
+    const template = window.Handlebars.templates['button.hbs'];
+    const html = template(this.#props);
+    this.#parent.insertAdjacentHTML('beforeend', html);
+    this.self.className = this.#style;
+    this.#handleClick();
+  }
 
-    /* Действие при нажатии */
-    #handleClick(){
-        if (this.#onSubmit !== undefined) {
-            this.self.addEventListener("click", (event) => {
-                event.preventDefault(); // Отменяем дефолтное дейтсвие
-                this.#onSubmit();       // Вызываем новое
-            });
-        }
-    }    
-
-    /* Рендер */
-    render(){
-        const template = window.Handlebars.templates["button.hbs"];
-        const html = template(this.#props);
-        this.#parent.insertAdjacentHTML("beforeend", html);
-        this.self.className = this.#style;
-        this.#handleClick();
+  /* При удалении объекта */
+  /*
+  #destructor() {
+    if (this.#onSubmit !== undefined) {
+      this.self.removeEventListener('click', (event) => {
+        event.preventDefault(); // Отменяем дефолтное дейтсвие
+        this.#onSubmit(); // Вызываем новое
+      });
     }
-
-    /* При удалении объекта */
-    #destructor(){
-        if (this.#onSubmit !== undefined) {
-            this.self.removeEventListener("click", (event) => {
-                event.preventDefault(); // Отменяем дефолтное дейтсвие
-                this.#onSubmit();       // Вызываем новое
-            });
-        }
-    } 
+  }
+    */
 }
--- public/src/components/input/input.js
+++ public/src/components/input/input.js
@@ -1,21 +1,22 @@
 /* Поле ввода */
 export class input {
-    #parent;                // Родитель (где вызывается)
-    #props = {              // Свойства поля ввода
-        id: "",             // Id для идентификации
-        placeholder: "",    // начальный текст
-    };
+  #parent; // Родитель (где вызывается)
+  #props = {
+    // Свойства поля ввода
+    id: '', // Id для идентификации
+    placeholder: '', // начальный текст
+  };
 
-    /* Конструктор */
-    constructor(parent, id, placeholder) {
-        this.#parent = parent;
-        this.#props = {id: id, placeholder: placeholder}
-    }
+  /* Конструктор */
+  constructor(parent, id, placeholder) {
+    this.#parent = parent;
+    this.#props = { id: id, placeholder: placeholder };
+  }
 
-    /* Рендер */
-    render(){
-        template = window.Handlebars.templates["input.hbs"];
-        html = template(this.#props);
-        this.#parent.insertAdjacentHTML("beforeend", html);
-    }
+  /* Рендер */
+  render() {
+    const template = window.Handlebars.templates['input.hbs'];
+    const html = template(this.#props);
+    this.#parent.insertAdjacentHTML('beforeend', html);
+  }
 }
