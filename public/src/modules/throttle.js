6937467973aedcb615fa16aaf8002231600d30ad
Jer <yaroslav.auto@gmail.com>
2025-03-07 23:54:13 +0300

    Add save for this

--- public/src/modules/throttle.js
+++ public/src/modules/throttle.js
@@ -1,22 +1,29 @@
 export default function throttle(func, ms) {
-    let isThrottled = false;
-    let savedArgs = null;
 
-    return (...args) => {
-        if (isThrottled) {
-            savedArgs = args;
+    let isThrottled = false,
+        savedArgs,
+        savedThis;
+
+    function wrapper() {
+
+        if (isThrottled) { // (2)
+            savedArgs = arguments;
+            savedThis = this;
             return;
         }
 
-        func.apply(this, args);
+        func.apply(this, arguments); // (1)
+
         isThrottled = true;
 
-        setTimeout(() => {
-            isThrottled = false;
+        setTimeout(function() {
+            isThrottled = false; // (3)
             if (savedArgs) {
-                func.apply(this, savedArgs);
-                savedArgs = null;
+                wrapper.apply(savedThis, savedArgs);
+                savedArgs = savedThis = null;
             }
         }, ms);
-    };
+    }
+
+    return wrapper;
 }
