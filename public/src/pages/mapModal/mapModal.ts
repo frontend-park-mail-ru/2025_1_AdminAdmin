401d6cf80ef23a3b53750dfdd9f6b6a4ddb05426
Jer <yaroslav.auto@gmail.com>
2025-04-03 16:40:55 +0300

    Update ymaps3 exports

--- public/src/pages/mapModal/mapModal.ts
+++ public/src/pages/mapModal/mapModal.ts
@@ -1,7 +1,14 @@
 import { SuggestsContainer } from '../../components/suggestsContainer/suggestsContainer';
 import { Button } from '../../components/button/button';
 import template from './mapModal.hbs';
-import { YMap, YMapDefaultSchemeLayer } from '../../lib/ymaps';
+import {
+  YMap,
+  YMapDefaultSchemeLayer,
+  YMapDefaultFeaturesLayer,
+  YMapControls,
+  YMapListener,
+  YMapMarker,
+} from '../../lib/ymaps';
 import {
   geoCoderRequest,
   geoCoderRequestByCoords,
@@ -100,9 +107,9 @@ export default class MapModal {
     });
 
     this.map.addChild(new YMapDefaultSchemeLayer({}));
-    this.map.addChild(new ymaps3.YMapDefaultFeaturesLayer({ zIndex: 1800 }));
+    this.map.addChild(new YMapDefaultFeaturesLayer({ zIndex: 1800 }));
 
-    this.controls = new ymaps3.YMapControls({
+    this.controls = new YMapControls({
       position: 'left',
       orientation: 'vertical',
     });
@@ -135,7 +142,7 @@ export default class MapModal {
 
     this.map.addChild(this.controls);
 
-    const mapListener = new ymaps3.YMapListener({
+    const mapListener = new YMapListener({
       layer: 'any',
       onClick: this.clickCallback.bind(this),
     });
@@ -214,7 +221,7 @@ export default class MapModal {
       this.map.removeChild(this.marker);
     }
 
-    this.marker = new ymaps3.YMapMarker(
+    this.marker = new YMapMarker(
       {
         coordinates: [lon, lat],
       },
