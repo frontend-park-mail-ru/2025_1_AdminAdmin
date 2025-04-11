edfcf71103789c89b8815981dff6f759c810ee3e
Jer <yaroslav.auto@gmail.com>
2025-04-11 12:48:57 +0300

    Add input restrictions

--- public/src/pages/orderPage/orderPageConfig.ts
+++ public/src/pages/orderPage/orderPageConfig.ts
@@ -3,22 +3,28 @@ export default {
     flat: {
       id: 'form__line__flat__input',
       placeholder: 'Кв./Офис',
-      type: 'number',
+      type: 'text',
+      maxLength: 6,
     },
     doorPhone: {
       id: 'form__line__door_phone__input',
       placeholder: 'Домофон',
       type: 'text',
+      maxLength: 10,
     },
     porch: {
       id: 'form__line__porch__input',
       placeholder: 'Подъезд',
       type: 'number',
+      min: 1,
+      max: 20,
     },
     floor: {
       id: 'form__line__floor__input',
       placeholder: 'Этаж',
       type: 'number',
+      min: 1,
+      max: 100,
     },
   },
   commentInput: {
