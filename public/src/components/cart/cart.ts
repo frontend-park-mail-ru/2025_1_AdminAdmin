d8d600e67e133031395ea3caf1167207be3936ec
Jer <yaroslav.auto@gmail.com>
2025-04-22 11:50:11 +0300

    fix

--- public/src/components/cart/cart.ts
+++ public/src/components/cart/cart.ts
@@ -75,7 +75,7 @@ export default class Cart {
     try {
       await cartStore.clearCart();
     } catch (error) {
-      toasts.error(error.error);
+      toasts.error(error.error || error.message);
     } finally {
       bin.style.pointerEvents = '';
     }
