726fd91f1f1c5b7a908eb396d647d75ac0bfe57f
Jer <yaroslav.auto@gmail.com>
2025-05-20 11:36:54 +0300

    Fix password error

--- public/src/components/qrModal/qrModal.ts
+++ public/src/components/qrModal/qrModal.ts
@@ -24,43 +24,18 @@ export class QRModal {
     document.body.insertAdjacentHTML('beforeend', html);
     document.body.style.overflow = 'hidden';
 
-    const blocker = document.createElement('div');
-    blocker.classList.add('qr_modal__blocker');
-    this.self.appendChild(blocker);
-
     const buttonContainer: HTMLElement = this.self.querySelector('.form__line');
 
     this.submitBtn = new Button(buttonContainer, {
       id: 'qr_modal__submit',
       style: 'dark form__button',
-      text: `Подождите ${this.delaySeconds} сек`,
-      disabled: true,
+      text: `Я сохранил QR код`,
       onSubmit: async () => {
         modalController.closeModal();
       },
     });
 
     this.submitBtn.render();
-
-    this.startCountdown(blocker);
-  }
-
-  private startCountdown(blocker: HTMLElement) {
-    let secondsLeft = this.delaySeconds;
-
-    const interval = setInterval(() => {
-      secondsLeft--;
-
-      if (secondsLeft > 0) {
-        this.submitBtn.setText(`Подождите ${secondsLeft} сек`);
-      } else {
-        clearInterval(interval);
-        this.submitBtn.setText('Я сохранил QR-код');
-        this.submitBtn.enable();
-
-        blocker.remove();
-      }
-    }, 1000);
   }
 
   remove() {
